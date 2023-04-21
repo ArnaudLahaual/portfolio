bbd4ef4732f5d16a14a28406ba37884abfb52d45
Arnaud <arnaud.lahaual@gmail.com>
2023-04-21 20:17:20 +0200

    projects terminé

---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import neolys from '../assets/neolys.png';
+import pokedex from '../assets/pokemon.jpg';
+
+const Projects = () => {
+    return(
+        <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+            <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
+            <p className="text-center py-8">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et quasi impedit, incidunt tempore odio distinctio eaque molestias necessitatibus quibusdam? Inventore neque nulla aut dolorum eos maxime nihil cumque labore sunt, dolore, autem totam incidunt quisquam quae ipsa ducimus accusamus eveniet recusandae! Facere veritatis ea eius ad dignissimos odio aspernatur omnis!
+            </p>
+            <div className="grid sm:grid-cols-2 gap-12">
+                <ProjectItem img={neolys} title='Neolys Patrimoine' />
+                <ProjectItem img={pokedex} title='Pokedex' /> 
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
