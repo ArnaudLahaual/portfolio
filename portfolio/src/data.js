de5035d3aae77b1a553861b49b470342a098fb90
Arnaud <arnaud.lahaual@gmail.com>
2023-04-21 16:26:01 +0200

    mise en place des data et du map pour affichage

---
+++ portfolio/src/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        year:2012,
+        title: 'Sixt',
+        duration: '8 ans',
+        details:'Conseiller Commercial'
+    },
+
+    {
+        year:2020,
+        title: 'Studi',
+        duration: '2 ans',
+        details:'Conseiller Commercial'
+    },
+
+    {
+        year:2022,
+        title: "O'Clock",
+        duration: '6 mois',
+        details:'Formation Developpeur web'
+    },
+]
