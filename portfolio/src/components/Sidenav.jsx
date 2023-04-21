2a6da07c1e328914f7f009550411f289b2a6ab06
Arnaud <arnaud.lahaual@gmail.com>
2023-04-21 14:45:23 +0200

    menu navbar mobile mis en place

--- portfolio/src/components/Sidenav.jsx
+++ portfolio/src/components/Sidenav.jsx
@@ -1,5 +1,11 @@
 import React, {useState} from "react";
-import {AiOutlineMenu, AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from 'react-icons/ai'
+import {GrProjects} from 'react-icons/gr'
+import {BsPerson} from 'react-icons/bs'
+import {MdOutlineWorkOutline} from 'react-icons/md';
+import {GiSkills} from 'react-icons/gi';
+
+
 
 const Sidenav = () =>{
 
@@ -13,25 +19,29 @@ const Sidenav = () =>{
             <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden"/>
             {
                 nav ? (
-                    <div className="fixed w-full h-sceen bg-white/90 flex flex flex-col justify-center items-center z-20">
+                    <div className="fixed w-full h-screen bg-white/90 flex flex flex-col justify-center items-center z-20">
                         <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration">
                         <AiOutlineHome size={20} />
                         <span className="pl-4">Home</span>
                         </a>
                         <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration">
-                        <AiOutlineHome size={20} />
+                        <GrProjects size={20} />
                         <span className="pl-4">Experiences</span>
                         </a>
                         <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration">
-                        <AiOutlineHome size={20} />
+                        <BsPerson size={20} />
+                        <span className="pl-4">Resume</span>
+                        </a>
+                        <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration">
+                        <AiOutlineProject size={20} />
                         <span className="pl-4">Projets</span>
                         </a>
                         <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration">
-                        <AiOutlineHome size={20} />
+                        <GiSkills size={20} />
                         <span className="pl-4">Compétences</span>
                         </a>
                         <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration">
-                        <AiOutlineHome size={20} />
+                        <AiOutlineMail size={20} />
                         <span className="pl-4">Contact</span>
                         </a>
                     </div>
